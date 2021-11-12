9645d01e27840bf2a0e2654574f368f5903a99a4
Jarvis Fitzgerald <Jarvis.Fitzgerald@Purplebricks.com>
2021-11-12 12:44:20 +0000

    fix issue with navbar padding click

--- src/Pages/navbar.js
+++ src/Pages/navbar.js
@@ -5,19 +5,11 @@ const NavBar = () => {
     return(
         <nav>
             <div class="topnav">
-                <a class="active" href="#home">
-                    <Link to="/">Home</Link>
-                </a>
-                <a href="#PremierLeague">
-                    <Link to="/PremierLeague">Premier League</Link>
-                </a>
-                <a href="#Login">
-                    <Link to="/Login">Login</Link>
-                </a>
-                <a href="#about">
-                    <Link to="/About">About</Link>
-                </a>
-                </div>
+                <Link class="homeLink" to="/">Home</Link>
+                <Link class="navLink" to="/PremierLeague">Premier League</Link>
+                <Link class="navLink" to="/Login">Login</Link>
+                <Link class="navLink" to="/About">About</Link>
+            </div>
         </nav>
     )
 }
